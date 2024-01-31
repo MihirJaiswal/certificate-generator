f86203693c712d46f4a50c1d035bc62447374c18
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-02-01 02:17:08 +0530

    some changes

--- certificate-generator/src/components/Footer.jsx
+++ certificate-generator/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   const year = new Date().getFullYear();
 
-  return <footer>{`Copyright © Certificate Generator ${year}`}</footer>;
+  return <footer>{`Copyright © Mihir Jaiswal ${year}`}</footer>;
 };
 
 export default Footer;
